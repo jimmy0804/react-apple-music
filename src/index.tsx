8aa17035964c29582b38925fc9909159ddd56cfe
Jimmy Yeung <jimmy.yeung@goxip.com>
2019-01-23 18:14:04 +0800

    Clean up

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,16 @@ import ReactDOM from 'react-dom';
 import App from './pages/App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
+const renderApp = () => ReactDOM.render(
   <App />, 
   document.getElementById('root') as HTMLElement
-);
+)
+
+// if (process.env.NODE_ENV !== 'production' && module.hot) {
+//   module.hot.accept('./pages/App', renderApp)
+// }
+
+renderApp()
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
