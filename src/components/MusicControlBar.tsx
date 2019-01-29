b3b924bacd55c3d8aa8f8424ca0fd97dcf70f5a6
Jimmy Yeung <jimmy.yeung@goxip.com>
2019-01-29 18:55:02 +0800

    Add style to music control bar

--- src/components/MusicControlBar.tsx
+++ src/components/MusicControlBar.tsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import { makeStyles } from '@material-ui/styles';
 import Grid from '@material-ui/core/Grid';
 
+const useStyles = makeStyles({
+  root: {
+    flexGrow: 1,
+    height: 70,
+    backgroundColor: 'white',
+    borderTop: '1px solid #eee;',
+    borderBottom: '1px solid #eee;'
+  },
+});
+
 const MusicControlBar = () => {
+  const classes = useStyles();
+
   return (
-    <Grid container spacing={0}>
+    <Grid container className={classes.root} spacing={0}>
       
     </Grid>
   )
