09d0c2def8bceb9555b0d23bc1f8fdf8442b1a0d
Jimmy Yeung <jimmy.yeung@goxip.com>
2019-01-29 18:54:45 +0800

    Add more base pages

--- src/pages/MainRoutes.tsx
+++ src/pages/MainRoutes.tsx
@@ -3,12 +3,16 @@ import { Switch, Route, Redirect } from 'react-router'
 
 import SettingsComponent from './settings/SettingsComponent'
 import LibraryComponent from './library/LibraryComponent'
+import BrowseComponent from './browse/BrowseComponent'
+import AccountComponent from './account/AccountComponent'
 
 const MainRoutes = () => {
   return (
     <Switch>
-      <Route exact path="/library" component={LibraryComponent} />
-      <Route exact path="/settings" component={SettingsComponent} />
+      <Route path="/library" component={LibraryComponent} />
+      <Route path="/browse" component={BrowseComponent} />
+      <Route path="/account" component={AccountComponent} />
+      <Route path="/settings" component={SettingsComponent} />
       <Route>
         <Redirect to="/library" />
       </Route>
---
+++ src/pages/account/AccountComponent.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AccountComponent = () => {
+  return (
+    <div>
+      <h1>Account</h1>
+    </div>
+  )
+}
+
+export default AccountComponent
